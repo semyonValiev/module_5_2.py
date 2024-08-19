c430dba8de6a99b8e1424702e0667bc14c617325
semyonValiev <semavali200@gmail.com>
2024-08-19 17:15:25 +0300

    Update module_5_2.py

--- module_5_2.py
+++ module_5_2.py
@@ -1,11 +1,19 @@
-class House():
-    def __init__(self):
-        self.number_of_floors = 0
+class House:
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
 
-    def set_new_number_of_floors(self, floor):
-        self.number_of_floors = floor
-        print(f'Номер этажа: {self.number_of_floors}')
+    def __len__(self):
+        return self.number_of_floors
 
+    def __str__(self):
+        return f"Название: {self.name}, количество этажей: {self.number_of_floors}"
 
-floor = House()
-floor.set_new_number_of_floors(6)
+h1 = House("ЖК Эльбрус", 10)
+h2 = House("ЖК .Акация", 20)
+# __str__
+print(h1)
+print(h2)
+#__len__
+print(len(h1))
+print(len(h2))
